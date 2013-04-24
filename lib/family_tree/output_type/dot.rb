29a098c21d6dbbed4254290d4c0d545f34d4e0fd
txemagon / imasen <txema.gonz@gmail.com>
2013-04-24 17:54:27 +0200

    Generation levels added to the diagram

--- lib/family_tree/output_type/dot.rb
+++ lib/family_tree/output_type/dot.rb
@@ -8,18 +8,22 @@ module FamilyTree
 
       # Use @output instance variable as IO system
       def process_relationship(node)
-        @output.puts "subgraph Marriage_#{node.id}{ \n"
+
+        @output.puts "{rank=same;\n" 
+        node.member.each do |m|
+          @output.puts "#{m.full_name};\n"
+        end
+        @output.puts "R_#{node.id};\n"
+        @output.puts "};\n"
         super do |node|
           @output.puts "\tR_#{node.id} [label=\"\" shape=\"point\"];"
         end
-        @output.puts "\n}"
       end
 
 
       def process_children(node)
-        @output.puts "subgraph Children_#{node.relationship.id}{ \n"
+        @output.puts "{rank=same;\n #{node.collect{ |s| "#{s.full_name};" }.join(";\n") } };\n"
         super
-        @output.puts "\n}"
       end
 
 
@@ -28,7 +32,7 @@ module FamilyTree
           @output.puts "\t#{node.full_name} [label=\"#{node.name}\"];"
           @output.puts "\t#{node.full_name} -> R_#{node.coming_from.id} ;" if node.coming_from
           node.marriages.each do |m|
-            @output.puts "\t#{node.full_name} -> R_#{m.id};"
+            @output.puts "\t#{node.full_name} -> R_#{m.id}[arrowsize=0];"
           end
         end
       end
